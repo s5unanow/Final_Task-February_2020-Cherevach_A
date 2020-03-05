1fb425dd64ee3458b38c8c770e08c0e1c0c188d8
s5unanow <s5unanow@gmail.com>
2020-03-05 13:27:23 +0200

    implemented part of slider v2

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,35 @@
 "use strict";
 
+class CycledArray {
+  constructor(array, startIndex = 0) {
+    this.array = array;
+    this.currentIndex = startIndex;
+  }
+  getCurrent () {
+    return this.array[this.currentIndex];
+  }
+  moveUp() {
+    if (this.array.length < this.currentIndex + 1) {
+      this.currentIndex = 0
+    } else {
+      this.currentIndex++;
+    }
+    return this.array[this.currentIndex];
+  }
+  moveDown() {
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.array.length - 1;
+    } else {
+      this.currentIndex--;
+    }
+    return this.array[this.currentIndex];
+  }
+}
+
 class Slider {
   constructor() {
-    this.itemsLeft = BagItem.createItemsFromID(window.bestOffer.left);
-    this.itemsRight = BagItem.createItemsFromID(window.bestOffer.right);
-    this.currentLeftItemNumber = 0;
-    this.currentRightItemNumber = 0;
+    this.itemsLeft = new CycledArray(BagItem.createItemsFromID(window.bestOffer.left));
+    this.itemsRight = new CycledArray(BagItem.createItemsFromID(window.bestOffer.right));
   }
   switchItem(switcherClass) {
     if (switcherClass.indexOf("best-offer__slide-up--left") >= 0) {
@@ -13,29 +37,44 @@ class Slider {
     } else {
       this.switchRightItem(switcherClass);
     }
+    this.updatePrice();
   }
   switchLeftItem(switcherClass) {
-    let DOMItem =
+    let DOMItem = document.querySelector(".slider-left");
     if (switcherClass.indexOf("best-offer__slide-up") >= 0) {
-      this.switchLeftItemUp(switcherClass);
+      this.switchLeftItemUp(DOMItem);
     } else {
-      this.switchRightItem(switcherClass);
+      this.switchLeftItemDown(DOMItem);
     }
   }
-  switchLeftItemUp() {
+  switchLeftItemUp(DOMItem) {
 
   }
-  switchLeftItemDown() {
+  switchLeftItemDown(DOMItem) {
 
   }
   switchRightItem(switcherClass) {
+    let DOMItem = document.querySelector(".slider-right");
+    if (switcherClass.indexOf("best-offer__slide-up") >= 0) {
+      this.switchRightItemUp(DOMItem);
+    } else {
+      this.switchRightItemDown(DOMItem);
+    }
+  }
+  switchRightItemUp(DOMItem) {
 
   }
+  switchRightItemDown(DOMItem) {
 
+  }
+  updatePrice() {
+    let price = this.itemsLeft.getCurrent().price + this.itemsRight.getCurrent().price;
+    let DOMPrice = document.querySelector(".best-offer__old-price");
+  }
   getCurrentItems() {
     let result = [];
-    let leftItem = Object.assign({}, this.itemsLeft[this.currentLeftItemNumber]);
-    let rightItem = Object.assign({}, this.itemsRight[this.currentRightItemNumber]);
+    let leftItem = Object.assign({}, this.itemsLeft.getCurrent());
+    let rightItem = Object.assign({}, this.itemsRight.getCurrent());
     result.push(leftItem);
     result.push(rightItem);
     return result
